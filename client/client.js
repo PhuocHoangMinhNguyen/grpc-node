569938a91695f5ec70d036d1797bfa2cc78e9768
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2022-01-02 23:26:36 +1100

    Update

--- client/client.js
+++ client/client.js
@@ -1,14 +1,65 @@
-var grpc = require("grpc");
-var services = require("../server/protos/dummy_grpc_pb");
+var greets = require("../server/protos/greet_pb");
+var service = require("../server/protos/greet_grpc_pb");
 
-function main() {
-  console.log("Hello from Client");
-  var client = new services.DummyServiceClient(
-    "localhose:50051",
+var calc = require("../server/protos/calculator_pb");
+var calcService = require("../server/protos/calculator_grpc_pb");
+
+var grpc = require("grpc");
+
+function callGreeting() {
+  var client = new service.GreetServiceClient(
+    "localhost:50051",
     grpc.credentials.createInsecure()
   );
 
-  // we do stuffs
-  console.log("client: ", client);
+  // create our request
+  var request = new greets.GreetRequest();
+
+  // create a protocol buffer
+  var greeting = new greets.Greeting();
+  greeting.setFirstName("Jerry");
+  greeting.setLastName("Tom");
+
+  request.setGreeting(greeting);
+
+  client.greet(request, (error, response) => {
+    if (!error) {
+      console.log("Greeting Response: ", response.getResult());
+    } else {
+      console.error(error);
+    }
+  });
+}
+
+function callSum() {
+  var client = new calcService.CalculatorServiceClient(
+    "localhost:50051",
+    grpc.credentials.createInsecure()
+  );
+
+  // create our request
+  var sumRequest = new calc.SumRequest();
+
+  sumRequest.setFirstNumber(10);
+  sumRequest.setSecondNumber(15);
+
+  client.sum(sumRequest, (error, response) => {
+    if (!error) {
+      console.log(
+        sumRequest.getFirstNumber() +
+          " + " +
+          sumRequest.getSecondNumber() +
+          " = " +
+          response.getSumResult()
+      );
+    } else {
+      console.error(error);
+    }
+  });
+}
+
+function main() {
+  callGreeting();
+  callSum();
 }
 main();
--- server/index.js
+++ server/index.js
@@ -1,8 +1,37 @@
+var greets = require("../server/protos/greet_pb");
+var service = require("../server/protos/greet_grpc_pb");
+
+var calc = require("../server/protos/calculator_pb");
+var calcService = require("../server/protos/calculator_grpc_pb");
+
 var grpc = require("grpc");
+
+/*
+  Implement the greet RPC method
+*/
+function greet(call, callback) {
+  var greeting = new greets.GreetResponse();
+  greeting.setResult(
+    "Hello " +
+      call.request.getGreeting().getFirstName() +
+      " " +
+      call.request.getGreeting().getLastName()
+  );
+  callback(null, greeting);
+}
+
+function sum(call, callback) {
+  var sumResponse = new calc.SumResponse();
+  sumResponse.setSumResult(
+    call.request.getFirstNumber() + call.request.getSecondNumber()
+  );
+  callback(null, sumResponse);
+}
 
 function main() {
   var server = new grpc.Server();
-
+  server.addService(service.GreetServiceService, { greet: greet });
+  server.addService(calcService.CalculatorServiceService, { sum: sum });
   server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
   server.start();
 
