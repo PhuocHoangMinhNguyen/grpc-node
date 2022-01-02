3801ba3ac2dc26eb2012422b69b39885e9921e63
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2022-01-02 17:30:34 +1100

    Dynamically Code Generation

--- client/client.js
+++ client/client.js
@@ -1,14 +1,82 @@
-var grpc = require("grpc");
-var services = require("../server/protos/dummy_grpc_pb");
+const path = require("path");
+const protoLoader = require("@grpc/proto-loader");
+const grpc = require("grpc");
+
+// grpc service definition for greet
+const greetProtoPath = path.join(__dirname, "..", "protos", "greet.proto");
+const greetProtoDefinition = protoLoader.loadSync(greetProtoPath, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+const greetPackageDefinition =
+  grpc.loadPackageDefinition(greetProtoDefinition).greet;
+
+const greetClient = new greetPackageDefinition.GreetService(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+);
+
+// grpc service definition for calculator
+const calcProtoPath = path.join(__dirname, "..", "protos", "calculator.proto");
+const calcProtoDefinition = protoLoader.loadSync(calcProtoPath, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+const calcPackageDefinition =
+  grpc.loadPackageDefinition(calcProtoDefinition).calculator;
+
+const calcClient = new calcPackageDefinition.CalculatorService(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+);
+
+function callGreeting() {
+  const request = {
+    greeting: {
+      first_name: "Jerry",
+      last_name: "Tom",
+    },
+  };
+  greetClient.greet(request, (error, response) => {
+    if (!error) {
+      console.log("Greeting Response: ", response.result);
+    } else {
+      console.error(error);
+    }
+  });
+}
+
+function callSum() {
+  const sumRequest = {
+    first_number: 10,
+    second_number: 15,
+  };
+
+  calcClient.sum(sumRequest, (error, response) => {
+    if (!error) {
+      console.log(
+        sumRequest.first_number +
+          " + " +
+          sumRequest.second_number +
+          " = " +
+          response.sum_result
+      );
+    } else {
+      console.error(error);
+    }
+  });
+}
 
 function main() {
-  console.log("Hello from Client");
-  var client = new services.DummyServiceClient(
-    "localhose:50051",
-    grpc.credentials.createInsecure()
-  );
-
-  // we do stuffs
-  console.log("client: ", client);
+  callGreeting();
+  callSum();
 }
 main();
--- server/index.js
+++ server/index.js
@@ -1,8 +1,57 @@
-var grpc = require("grpc");
+const path = require("path");
+const protoLoader = require("@grpc/proto-loader");
+const grpc = require("grpc");
+
+// grpc service definition for greet
+const greetProtoPath = path.join(__dirname, "..", "protos", "greet.proto");
+const greetProtoDefinition = protoLoader.loadSync(greetProtoPath, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+const greetPackageDefinition =
+  grpc.loadPackageDefinition(greetProtoDefinition).greet;
+
+// grpc service definition for calculator
+const calcProtoPath = path.join(__dirname, "..", "protos", "calculator.proto");
+const calcProtoDefinition = protoLoader.loadSync(calcProtoPath, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+const calcPackageDefinition =
+  grpc.loadPackageDefinition(calcProtoDefinition).calculator;
+
+/*
+  Implement the greet RPC method
+*/
+function greet(call, callback) {
+  const firstName = call.request.greeting.first_name;
+  const lastName = call.request.greeting.last_name;
+
+  callback(null, { result: "Hello " + firstName + " " + lastName });
+}
+
+function sum(call, callback) {
+  const first_number = call.request.first_number;
+  const second_number = call.request.second_number;
+  callback(null, { sum_result: first_number + second_number });
+}
 
 function main() {
-  var server = new grpc.Server();
-
+  const server = new grpc.Server();
+  server.addService(greetPackageDefinition.GreetService.service, {
+    greet: greet,
+  });
+  server.addService(calcPackageDefinition.CalculatorService.service, {
+    sum: sum,
+  });
   server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
   server.start();
 
